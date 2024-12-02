ca588b9f2275de34a7bf7f537df59258f0613984
Laisa Muito Linda <laisa.rioverde@gmail.com>
2024-12-02 15:24:01 -0300

    wip

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import _axios from 'axios'
 
 const axios = _axios.create({
     // baseURL: 'http://192.168.0.6:8000/',
-    baseURL: 'https://30f1-189-84-180-54.ngrok-free.app',
+    baseURL: 'https://5ece-189-84-180-54.ngrok-free.app',
     // baseURL: 'http://192.168.0.1:8000/',
     withCredentials: false,
     headers: {
