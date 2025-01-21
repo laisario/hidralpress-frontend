a1eea4cd1698747094797ad91a6a8a2c4d67bcd6
LP <lp@tsl.io>
2025-01-20 21:07:47 -0300

    alteirei baseurl

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import _axios from 'axios'
 
 const axios = _axios.create({
     // baseURL: 'http://192.168.0.6:8000/',
-    baseURL: 'https://4ba1-177-71-79-40.ngrok-free.app',
+    baseURL: 'https://hidralpress.ngrok.app',
     withCredentials: false,
     headers: {
         'Content-Type': 'multipart/form-data',
